f9e7fe41417cc9488ec4c4eaca1d1b9c1ada6c61
Aleksandra <semenovalekshu@gmail.com>
2021-09-28 18:51:23 +0300

    feat: add style and code for booking buttons

--- booking_ticket_form.js
+++ booking_ticket_form.js
@@ -1,3 +1,5 @@
+
+
 function openBookingForm() {
     document.querySelector(".booking-ticket-form").style.display = "block";
 }
@@ -5,3 +7,21 @@ function openBookingForm() {
 function closeBookingForm() {
     document.querySelector(".booking-ticket-form").style.display = "none";
 }
+
+// Basic tickets
+document.querySelector(".count-down-amount-basic").addEventListener("click", () => {
+    document.querySelector(".count-amount-basic").textContent === "0" ? null : document.querySelector(".count-amount-basic").innerHTML--;
+});
+
+document.querySelector(".count-up-amount-basic").addEventListener("click", () => {
+    document.querySelector(".count-amount-basic").innerHTML === "20" ? null : document.querySelector(".count-amount-basic").innerHTML++;
+});
+
+// Senior tickets
+document.querySelector(".count-down-amount-senior").addEventListener("click", () => {
+    document.querySelector(".count-amount-senior").textContent === "0" ? null : document.querySelector(".count-amount-senior").innerHTML--;
+});
+
+document.querySelector(".count-up-amount-senior").addEventListener("click", () => {
+    document.querySelector(".count-amount-senior").textContent === "20" ? null : document.querySelector(".count-amount-senior").innerHTML++;
+});
